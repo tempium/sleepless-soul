70359d77b32b0f74579b7b1c95fd2a234974c2c3
Benjamin Bureethan <bestcub4144@gmail.com>
2018-01-27 01:40:54 +0700

    Add objects

--- SleeplessSoul/Assets/Scripts/HolyObject.cs
+++ SleeplessSoul/Assets/Scripts/HolyObject.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DirectionGauge : MonoBehaviour {
+public class HolyObject : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float pushForce;
+
+    private Rigidbody2D rb;
+    private bool isBurn;
+    private bool canPossessed;
+    private PointEffector2D pointEffector;
+
+    // Use this for initialization
+    void Start()
+    {
+        isBurn = false;
+        canPossessed = false;
+        pointEffector = GetComponent<PointEffector2D>();
+        pointEffector.forceMagnitude = pushForce;
+
+    }
+
 }
