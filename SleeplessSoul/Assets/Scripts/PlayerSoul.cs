01303bad77a4b650f19be2e1a2bd7e5d52f809c1
munggumy <iknobkb@gmail.com>
2018-01-26 22:23:22 +0700

    add PlayerSoul

--- SleeplessSoul/Assets/Scripts/PlayerSoul.cs
+++ SleeplessSoul/Assets/Scripts/PlayerSoul.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerSoul : MonoBehaviour {
 
+    public float moveSpeed = 1;
+
+    private Rigidbody2D rb;
+    private GameObject body;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Vector2 direction = new Vector2(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y);
+            Move(direction.normalized);
+        }
 	}
+
+    void Move(Vector2 direction) {
+        rb.velocity = direction * moveSpeed;
+    }
 }
